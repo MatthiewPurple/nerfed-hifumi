9555781af000e7515fd013a7d9a65198e860f0e2
Matthiew Purple <matthiewpurple@hotmail.com>
2024-06-16 12:04:25 -0400

    Add project files.

---
+++ NerfedHifumi.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+using Il2Cpp;
+using nerfed_hifumi;
+
+[assembly: MelonInfo(typeof(NerfedHifumi), "Nerfed Hifumi", "1.0.0", "Matthiew Purple")]
+[assembly: MelonGame("アトラス", "smt3hd")]
+
+namespace nerfed_hifumi;
+public class NerfedHifumi : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        tblHearts.fclHeartsTbl[6].Skill[0].ID = 22;
+    }
+}
