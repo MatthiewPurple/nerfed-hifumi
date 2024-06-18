ceefbaf88710c39d1eb468ae283095a4f7198355
Matthiew Purple <matthiewpurple@hotmail.com>
2024-06-18 12:25:54 -0400

    new branch

--- NerfedHifumi.cs
+++ NerfedHifumi.cs
@@ -1,15 +1,19 @@
 ﻿using MelonLoader;
 using Il2Cpp;
-using nerfed_hifumi;
+using nerfed_magatamas;
 
-[assembly: MelonInfo(typeof(NerfedHifumi), "Nerfed Hifumi", "1.0.0", "Matthiew Purple")]
+[assembly: MelonInfo(typeof(NerfedHifumi), "Nerfed Magatamas", "1.0.0", "Matthiew Purple")]
 [assembly: MelonGame("アトラス", "smt3hd")]
 
-namespace nerfed_hifumi;
+namespace nerfed_magatamas;
 public class NerfedHifumi : MelonMod
 {
     public override void OnInitializeMelon()
     {
+        tblHearts.fclHeartsTbl[2].Skill[0].ID = 10;
+        tblHearts.fclHeartsTbl[5].Skill[0].ID = 4;
         tblHearts.fclHeartsTbl[6].Skill[0].ID = 22;
+        tblHearts.fclHeartsTbl[8].Skill[0].ID = 16;
+        tblHearts.fclHeartsTbl[11].Skill[2].ID = 194;
     }
 }
